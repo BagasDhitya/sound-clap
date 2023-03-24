277ed503882fcd5d149c46f817ad3995d086e7c9
Bagas Dhitya Taufiqqi <bagas@Bagass-MacBook-Air.local>
2023-03-24 15:29:30 +0700

    add recommended api and save key

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
+import Card from "../../components/Card";
+import axios from "axios";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [recommend, setRecommend] = useState([]);
+
+  async function getRecommendations() {
+    try {
+      const response = await axios.get(
+        "https://shazam.p.rapidapi.com/songs/list-recommendations",
+        {
+          params: {
+            key: "484129036",
+            locale: "en-US",
+          },
+          headers: {
+            "X-RapidAPI-Key": import.meta.env.VITE_SHAZAM_KEY,
+            "X-RapidAPI-Host": "shazam.p.rapidapi.com",
+          },
+        }
+      );
+      setRecommend(response.data.tracks);
+      console.log(response.data.tracks);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getRecommendations();
+  }, []);
+
+  return (
+    <Layout>
+      <div>
+        <p className="font-bold text-2xl m-10">Recommended This Month</p>
+        {recommend ? (
+          recommend.map((item: any, index) => {
+            return (
+              <div className="m-5">
+                <Card
+                  id={index}
+                  image={item.images.background}
+                  title={item.title}
+                  subtitle={item.subtitle}
+                />
+              </div>
+            );
+          })
+        ) : (
+          <></>
+        )}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
