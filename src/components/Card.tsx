1582b7667dceec18ebb729cb708429fca8112761
Bagas Dhitya Taufiqqi <bagas@Bagass-MacBook-Air.local>
2023-03-24 15:08:15 +0700

    restructure card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,19 +4,20 @@ interface Card {
   id: string | number;
   image?: string;
   title?: string;
+  subtitle?: string;
 }
 
-const Card: FC<Card> = ({ id, image, title }) => {
+const Card: FC<Card> = ({ id, image, title, subtitle }) => {
   return (
-    <div className="card card-side bg-white shadow-xl" key={id}>
+    <div className="card card-side bg-white" key={id}>
       <figure>
-        <img src={image} className="w-40 h-40 rounded-md ml-10" />
+        <img src={image} className="w-40 h-40 rounded-md ml-5" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
-        <p>Click the button to watch on Jetflix app.</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Watch</button>
+        <p>{subtitle}</p>
+        <div className="card-actions mt-10">
+          <button className="bg-orange-clap text-white">Watch</button>
         </div>
       </div>
     </div>
